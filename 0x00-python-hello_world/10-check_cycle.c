41460034dd40481c347fb367f19f1d9e091a698e
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-08 19:39:32 +0100

    done with 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a cycle
+ * @list: pointer to list's head
+ * Return: int
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *hare, *tortoise;
+
+	hare = list;
+	tortoise = list;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+
+		if (hare == tortoise)
+			return (1);
+	}
+
+	return (0);
+}
