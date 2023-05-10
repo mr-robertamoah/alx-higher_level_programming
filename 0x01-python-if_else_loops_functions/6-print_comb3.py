f03afe7802292ec3fdb6949987e9bb01e6265a4d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-10 05:16:09 +0100

    corrected 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,9 @@
 for i in range(9):
     j = i + 1
     while j < 10:
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j), end="")
+            break
         print("{}{}, ".format(i, j), end="")
         j += 1
 print("")
