eea24f6808d21cee948fdbd62ea3247ac1c5d23a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-11 08:23:55 +0100

    done with 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-pow = __import__('11-pow').pow
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+fizz =  __import__("12-fizzbuzz").fizzbuzz
+fizz()
+print("")
