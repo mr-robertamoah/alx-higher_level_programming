b1d2cd4eb09cb1812c39db7e687c33eca2bd44fd
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-13 19:02:51 +0100

    done with 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    number_of_elem = len(my_list)
+    if number_of_elem == 0 or idx < -1 or number_of_elem - 1 < idx:
+        return None
+
+    return my_list[idx]
