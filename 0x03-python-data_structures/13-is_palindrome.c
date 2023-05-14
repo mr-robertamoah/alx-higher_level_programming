8559656b961e2943b44b2e47288e4a06882fe7fb
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-14 09:08:17 +0100

    corrected 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * add_node - adds a new node at the beginning of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_node(listint_t **head, int n)
+{
+	listint_t *new;
+	listint_t *next;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		next = *head;
+		new->next = next;
+		(*head) = new;
+	}
+
+	return (new);
+}
+
 listint_t *reverse_list(listint_t *node)
 {
 	listint_t *head;
@@ -13,14 +43,15 @@ listint_t *reverse_list(listint_t *node)
 	{
 		tmp = node->next;
 		node->next = NULL;
-		add_nodeint_end(&head, node->n);
+		add_node(&head, node->n);
 	}
 	return (head);
 }
 
 /**
-* is_palindrome - 
-*
+* is_palindrome - determine whether or not a linked list
+* is palindrome or not
+* Return: int (0 or 1)
 */
 int is_palindrome(listint_t **head)
 {
