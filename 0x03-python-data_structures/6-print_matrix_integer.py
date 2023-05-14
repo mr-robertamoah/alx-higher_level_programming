50f3f1262defb3d1e593c545be13db9e3c82a855
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-14 03:32:48 +0100

    done with 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    if matrix_len == 0 or (matrix_len == 1 and len(matrix[0]) == 0):
+        print()
+
+    if isinstance(matrix, list):
+        for i in range(matrix_len):
+            if isinstance(matrix[i], list):
+                for j in range(len(matrix[i])):
+                    if j + 1 == len(matrix[i]):
+                        print("{}".format(matrix[i][j]))
+                    else:
+                        print("{} ".format(matrix[i][j]), end="")
