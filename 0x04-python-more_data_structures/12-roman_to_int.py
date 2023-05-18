30c44a97e948b2362403f23e6ae2d5267a7c468d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-18 07:07:56 +0100

    made corrections to 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,3 +7,23 @@ def roman_to_int(roman_string):
     hund_dict = {"CM": 900, "DCCC": 800, "DCC": 700, "DC": 600, "D": 500, "CD": 400, "CCC": 300, "CC": 200, "C": 100}
     tens_dict = {"XC": 90, "LXXX": 80, "LXX": 70, "LX": 60, "L": 50, "XL": 40, "XXX": 30, "XX": 20, "X": 10}
     ones_dict = {"IX": 9, "VIII": 8, "VII": 7, "VI": 6, "V": 5, "IV": 4, "III": 3, "II": 2, "I": 1}
+
+    sum_r = 0
+    for k, v in thous_dict.items():
+        if k in roman_string:
+            sum_r += v
+            break
+    for k, v in hund_dict.items():
+        if k in roman_string:
+            sum_r += v
+            break
+    for k, v in tens_dict.items():
+        if k in roman_string:
+            sum_r += v
+            break
+    for k, v in ones_dict.items():
+        if k in roman_string:
+            sum_r += v
+            break
+
+    return sum_r
