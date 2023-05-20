af5c2f7b56e99222f4f6b5d772d7f94f1f63642c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-20 07:28:06 +0100

    made corrections to 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,10 @@
 
 def complex_delete(a_dictionary, value):
     if isinstance(a_dictionary, dict):
+        del_list = []
         for k, v in a_dictionary.items():
             if v == value:
-                del a_dictionary[k]
+                del_list.append(k)
+
+        for k in del_list:
+            del a_dictionary[k]
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -3,8 +3,8 @@ complex_delete = __import__('102-complex_delete').complex_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-new_dict = complex_delete(a_dictionary, 'C')
+a_dictionary = {'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e"} 
+new_dict = complex_delete(a_dictionary, 'a')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
