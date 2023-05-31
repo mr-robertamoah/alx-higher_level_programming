2bedeed1b9ed56578c2169a2fa51b551fe80756a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-31 17:09:40 +0100

    done adding docstrings to the modules and classes of the 0-square.py and 1-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
+"""
+This is a module that has a Square class
+
+"""
+
+
 class Square:
+    """
+    This is an class with no attributes or methods
+
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+"""
+This module contains a Square class
+
+"""
+
+
 class Square:
+    """
+    This is a class representing a square
+
+    Attributes:
+        __size (int): this is an integer representing the length of a side
+    """
     def __init__(self, size):
+        """
+        This is called when the class is initialized
+
+        Args:
+            size (int): this is used to initialize the __size private attribute
+        """
         self.__size = size
