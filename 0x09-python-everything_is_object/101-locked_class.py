7281f1b9ea888d9ea73cefa88fc1a6552fa7302f
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-06 13:44:39 +0100

    done with 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Contains a LockedClass which only allows a specific
+instance attribute
+
+"""
+
+
+class LockedClass:
+    """
+    Allows only first_name attribute to set to its instances
+
+    """
+    __slots__ = ('first_name', )
+
+    def __init__(self):
+        """ Initializes the object """
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        """ Overrides the __setattr__ """
+
+        if name != 'first_name':
+            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+        super().__setattr__(name, value)
