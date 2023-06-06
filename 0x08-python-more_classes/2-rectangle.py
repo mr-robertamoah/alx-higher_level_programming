d2b9d8f5af96fb5df1efa2886a39bb60bf2f27a9
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-06 05:58:22 +0100

    made corrections to the perimeter methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -95,4 +95,7 @@ class Rectangle():
         Returns:
             int: results of adding the length of all four sides
         """
+        if self.__width < 0 or self.__height < 0:
+            return 0
+
         return 2 * (self.__width + self.__height)
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -97,6 +97,9 @@ class Rectangle():
         Returns:
             int: results of adding the length of all four sides
         """
+        if self.__width < 0 or self.__height < 0:
+            return 0
+
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
