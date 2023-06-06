5378ee0648bddc932f807ca9327eba795569e45c
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-06 05:52:24 +0100

    made changes to the __init__ function of all the rectangle files

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -21,6 +21,18 @@ class Rectangle():
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
         Rectangle.number_of_instances += 1
         self.__height = height
         self.__width = width
