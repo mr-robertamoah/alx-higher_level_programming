801edd62ad83b54a266b1b26e069a8ba341aa470
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-06 07:52:54 +0100

    made corrections to 9-rectangle.py

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -4,3 +4,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
+try:
+    mysquare = Rectangle.square(-2)
+    print("{} / {}".format(mysquare.width, mysquare.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
