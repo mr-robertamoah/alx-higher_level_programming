3b8c4133e132279219ebb6937e78295c7d6c2b1d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-06 18:11:00 +0100

    made more corrections to 9-rectangle.py

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -3,7 +3,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+new_square = eval(repr(my_square))
 print(my_square)
+print("--")
+print(new_square)
 try:
     mysquare = Rectangle.square(-2)
     print("{} / {}".format(mysquare.width, mysquare.height))
