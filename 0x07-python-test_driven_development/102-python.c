c53b271b92b63012558169f09edd4f1e0c6c4b73
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-11 21:43:01 +0100

    done with 102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,28 @@
+#include "Python.h"
+
+/**
+ * print_python_string - prints Python strings information
+ * @p: PyObject pointer.
+ */
+void print_python_string(PyObject *p)
+{
+	long int len;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	len = ((PyASCIIObject *) (p))->length;
+
+	if (PyUnicode_KIND(obj) == PyUnicode_1BYTE_KIND)
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", len);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+}
