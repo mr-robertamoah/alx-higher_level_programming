57b4668c2a3d225506a4cac92772d65266545995
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-12 10:00:33 +0100

    done with 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Contains the lookup function that returns attributes
+of an object
+"""
+
+def lookup(obj):
+    """
+    Returns the all available attributes on obj
+
+    Args:
+        obj (<class object>); python object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,10 @@
+# HIGHER LEVEL PROGRAMMING
+
+## Python Inheritance
+
+This deals with base class inheriting super class.
+It also deals with the use of methods such as:
+- isinstance
+- type
+- super
+- etc
