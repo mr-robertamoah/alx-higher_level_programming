cbf2034400bed696b6cd2da6b33930e7d08a2de1
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-12 16:34:15 +0100

    done with 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Contains a Square class
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square class extending the Rectangle class
+    """
+
+    def __init__(self, size):
+        """ Initializes private attributes """
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def area(self):
+        """ Calculates the area of the Square """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """ String representation of the Class"""
+        return f"[Square] {self.__size}/{self.__size}"
