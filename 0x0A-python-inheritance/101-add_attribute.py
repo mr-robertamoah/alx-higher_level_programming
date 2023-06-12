6ef6c10ce7a322ddf60afab02951c13943f408f2
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-12 19:46:51 +0100

    done with 101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Contains the add_attribute function
+"""
+
+
+def add_attribute(obj, name, value):
+    """ Tries to add an attribute to an object """
+    cls = obj.__class__
+    slots = getattr(cls, "__slots__", None)
+
+    if (slots and not name in slots) or\
+       (not hasattr(cls, "__setattr__")) or\
+       isinstance(obj, (int, float, str, tuple, frozenset)):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, name, value)
