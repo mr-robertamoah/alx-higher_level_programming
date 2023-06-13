20989e228c097206a074b759069f075b69d3fc3d
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-13 10:20:32 +0100

    done with 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Contains read_file function
+"""
+
+
+def read_file(filename=""):
+    """
+    Read a text file with UTF8 encoding
+
+    Args:
+        filename (str): name of or path to file
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,11 @@
+# HIGHER LEVEL PROGRAMMING
+
+## Python Input/Output
+
+This project deals with input an output operations
+to and from files. I involves:
+- opening files
+- reading from files
+- writing to files
+- dumping and loading JSON
+- etc
