a96768f59af7bcf27bb4753f11f6369858aa06f6
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-13 23:46:47 +0100

    done with 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Contains a Student class
+"""
+
+
+class Student():
+    """ Student Class with instance variables """
+
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation of instance variables """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns dictionary representation of Student """
+
+        if isinstance(attrs, list):
+            return {key: self.__dict__[key]
+                    for key in self.__dict__ if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = json[key]
