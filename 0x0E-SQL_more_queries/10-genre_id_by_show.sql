6e64cafd2acb648f1792a7a5d83f962840fb16b2
Robert Amoah <mr_robertamoah@yahoo.com>
2023-07-20 06:32:28 +0100

    done with 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- This lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- where each record should display: tv_shows.title - tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
