4e782ff1d0fce339e1bd2ab519231de9a07b5996
Robert Amoah <mr_robertamoah@yahoo.com>
2023-07-20 05:14:26 +0100

    done with 6-states.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- This creates the table force_name
-CREATE TABLE force_name (
+CREATE TABLE IF NOT EXISTS force_name (
 id INT,
 name VARCHAR(256) NOT NULL
 );
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- This creates the table id_not_null
-CREATE TABLE id_not_null  (
+CREATE TABLE IF NOT EXISTS id_not_null  (
 id INT DEFAULT 1,
 name VARCHAR(256)
 );
