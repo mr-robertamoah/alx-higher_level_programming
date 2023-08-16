514127d7ff4e71789aaa34d29b7b94f4848bc096
Robert Amoah <mr_robertamoah@yahoo.com>
2023-08-16 06:56:54 +0000

    made corrections to 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Sq = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Sq {
   charPrint (c) {
     if (c === undefined) {
       this.print();
