4d44178602dd8a3038db96260ee57815a23065e0
Robert Amoah <mr_robertamoah@yahoo.com>
2023-09-01 06:26:38 +0100

    made the number of lines in scripts 3

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response
 # A header variable X-School-User-Id must be sent with the value 98
-
 curl -s -H "X-HolbertonSchool-User-Id: 98" "${1}"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# sends a POST request to the passed URL, and displays the body of the response
-# A variable email must be sent with the value test@gmail.com
-# A variable subject must be sent with the value I will always be here for PLD
-
+# variables email and subject and their values must be sent with response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
